83111650b9d0672029a95ac41cda2e8bf1951d9e
eitan baron <eitanbaron@outlook.com>
2020-12-05 23:43:50 +0200

    1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { 
+  faUserSecret,
+  faTrashAlt
+ } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(
+  faUserSecret,
+  faTrashAlt
+  );
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
